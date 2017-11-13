7e7826260bf5abe22945e3c7f259778273185b87
ryan <rspurr@u.rochester.edu>
2017-11-12 20:49:14 -0500

    Changes to some imports && adds sphinx docs

--- actions/DayActions.py
+++ actions/DayActions.py
@@ -13,7 +13,7 @@ class DayActions:
     def check_appt(self, appt):
         """
         Checks if the appointment can be scheduled that day
-        :param appt:
+        :param: Appointment appt: appointment we want to schedule
         :return:
         """
         appt_timeslot = self.translate_time_to_slot(appt.time)
@@ -25,6 +25,12 @@ class DayActions:
             return False
 
     def translate_time_to_slot(self, time):
+        """
+        Translates a standard time i.e. "6:15" and turns it into a timeslot we can use for indexing
+        :param String time: time we want to convert
+        :return: int index the index of the timeslot in the schedule
+        """
+
         hour, minute = time.split(":")
 
         # hour * 4 indexes into the 4 entries for that hour,
